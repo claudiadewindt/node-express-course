5dd837874dd228bb59aaf0f766f2b002dc7e3262
Claudia <caucrah@gmail.com>
2021-10-02 01:32:52 +0200

    Finished express 1 and 2

--- 02-express-tutorial/final/01-http-basics.js
+++ 02-express-tutorial/final/01-http-basics.js
@@ -1,26 +1,32 @@
-const http = require('http')
+const http = require('http');
+const { readFileSync } = require('fs');
+
+//get all files
+const homePage = readFileSync('./index.html'); //should create a index.html file to show in the browser
 
 const server = http.createServer((req, res) => {
-  // console.log(req.method)
-  const url = req.url
+  // console.log(req.method) GET
+  const url = req.url; // console.log(req.url) /contact /about ...
   // home page
   if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>home page</h1>')
-    res.end()
+    res.writeHead(200, { 'content-type': 'text/html' }); // providing headers, metadata 'text/plain' will show like this <h1>home page</h1>
+    res.write(homePage);
+    res.end();
   }
   // about page
   else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>about page</h1>')
-    res.end()
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>about page</h1>');
+    res.end();
   }
   // 404
   else {
-    res.writeHead(404, { 'content-type': 'text/html' })
-    res.write('<h1>page not found</h1>')
-    res.end()
+    res.writeHead(404, { 'content-type': 'text/html' });
+    res.write('<h1>page not found</h1>');
+    res.end();
   }
-})
+});
 
-server.listen(5000)
+server.listen(5000);
+
+//port is the communication endpoint
